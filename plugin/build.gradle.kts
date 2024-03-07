7aa8989da7313d1f46bb574d035b0abf0e1d5588
KoblizekXD <janprokupek@centrum.cz>
2024-03-07 07:04:55 +0100

    Setup plugin publishing

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,7 +1,35 @@
 plugins {
-    java
+    `java-library`
+    `java-gradle-plugin`
+    `maven-publish`
 }
 
 dependencies {
     implementation(":api")
+}
+
+gradlePlugin {
+    plugins {
+        create("workbench") {
+            id = "dev.mudkip.workbench"
+            displayName = "Workbench"
+            description = "Gradle plugin for Minecraft"
+            implementationClass = "dev.mudkip.workbench.WorkbenchPlugin"
+            tags = listOf("minecraft", "modding", "decompilation", "deobfuscation", "amber", "amber-toolchain")
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = groupId
+            artifactId = "workbench-plugin"
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal()
+        // Add gradle plugin portal later here
+    }
 }
