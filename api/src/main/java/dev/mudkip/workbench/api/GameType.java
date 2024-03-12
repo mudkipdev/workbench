9b03eecb14d5dc27ed4544cc58be7730c47cb815
KoblizekXD <janprokupek@centrum.cz>
2024-03-12 20:14:58 +0100

    unfinished BetaCraftFormatSerializer

--- api/src/main/java/dev/mudkip/workbench/api/GameType.java
+++ api/src/main/java/dev/mudkip/workbench/api/GameType.java
@@ -1,8 +1,15 @@
 package dev.mudkip.workbench.api;
 
+import java.util.Arrays;
+
 public enum GameType {
     OLD_ALPHA,
     OLD_BETA,
     SNAPSHOT,
-    RELEASE
+    RELEASE;
+
+    public static GameType get(String name) {
+         return Arrays.stream(values()).filter(i -> i.toString().toLowerCase().equals(name))
+                 .findFirst().orElse(null);
+    }
 }
---
+++ api/src/main/java/dev/mudkip/workbench/api/versions/serializers/BetaCraftFormatSerializer.java
@@ -0,0 +1,91 @@
+package dev.mudkip.workbench.api.versions.serializers;
+
+import com.google.gson.*;
+import dev.mudkip.workbench.api.GameType;
+import dev.mudkip.workbench.api.versions.VersionData;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class BetaCraftFormatSerializer implements JsonSerializer<VersionData>, JsonDeserializer<VersionData> {
+    @Override
+    public JsonObject serialize(VersionData src, Type typeOfSrc, JsonSerializationContext context) {
+        return null;
+    }
+
+    @Override
+    public VersionData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject object = json.getAsJsonObject();
+        return new VersionData() {
+            @Override
+            public String getJson() {
+                return object.toString();
+            }
+
+            @Override
+            public String getMainClass() {
+                return object.getAsJsonPrimitive("mainClass").getAsString();
+            }
+
+            @Override
+            public ArgumentListing getMinecraftArgs() {
+                ArgumentListing args = new ArgumentListing();
+                for (JsonElement element : object.getAsJsonObject("arguments").getAsJsonArray("game").asList()) {
+                    String item = element.getAsString();
+                    if (item.contains("=")) {
+                        String[] split = item.split("=");
+                        args.put(split[0], split[1].replaceAll("[{}$]", ""));
+                    } else
+                        args.put(item, null);
+                }
+                return args;
+            }
+
+            @Override
+            public ArgumentListing getJvmArgs() {
+                ArgumentListing args = new ArgumentListing();
+                for (JsonElement element : object.getAsJsonObject("arguments").getAsJsonArray("jvm").asList()) {
+                    String item = element.getAsString();
+                    if (item.contains("=")) {
+                        String[] split = item.split("=");
+                        args.put(split[0], split[1].replaceAll("[{}$]", ""));
+                    } else
+                        args.put(item, null);
+                }
+                return args;
+            }
+
+            @Override
+            public List<Library> getLibraries() {
+                return null;
+            }
+
+            @Override
+            public String getId() {
+                return object.getAsJsonPrimitive("id").getAsString();
+            }
+
+            @Override
+            public GameType getType() {
+                return GameType.get(object.getAsJsonPrimitive("type").getAsString());
+            }
+
+            @Override
+            public Downloads getDownloads() {
+                return new Downloads(
+                        looping(object, "downloads.client").getAsJsonObject()
+                                .getAsJsonPrimitive("url").getAsString(),
+                        looping(object, "downloads.server").getAsJsonObject()
+                                .getAsJsonPrimitive("url").getAsString()
+                );
+            }
+        };
+    }
+
+    private static JsonElement looping(JsonObject obj, String path) {
+        for (String s : path.split("\\.")) {
+            obj = obj.getAsJsonObject(s);
+        }
+        return obj;
+    }
+}
